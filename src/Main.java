fa2e0fdcf2597d8f773c7780506998df78590646
Alec Day <shirojyuu@gmail.com>
2016-03-30 18:53:44 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Hello World!");
+        System.out.println("I was cloned!");
+
+        System.out.print("What's your name? Type it here:");
+        String name = s.next();
+
+        //Print Message with Name
+        System.out.println("It's nice to meet you, " + name);
+
+    }
+}
